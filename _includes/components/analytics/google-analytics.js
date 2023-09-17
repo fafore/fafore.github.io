ea93e1c82a7f5f9d84313740d225bc7cdc420d59
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2023-09-17 09:26:30 -0600

    Organize google analytics data

--- _includes/components/analytics/google-analytics.js
+++ _includes/components/analytics/google-analytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', '{{ site.analytics }}', 'auto');
+ga('create', '{{ site.components.analytics.google-analytics }}', 'auto');
 ga('send', 'pageview');
